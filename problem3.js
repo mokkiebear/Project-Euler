2465953dd1f8527a822059c8e30476b2d4b1bb37
Maxim Rubanov <mrubanau@gomel.iba.by>
2019-03-07 22:10:34 +0300

    The problem 3 is solved.

--- problem3.js
+++ problem3.js
@@ -4,21 +4,20 @@ What is the largest prime factor of the number 600851475143 ?
 */
 
 let num = 600851475143;
-let flag = true;
 let end = Math.round(Math.sqrt(num));
+let result = 1;
 for (let i = 1; i < end; i++){
-    if (num % i == 0){
-        for (let j = 2; j < i; j++){
-            if(isPrime(j)) console.log(i);
+    if (num % i == 0 && isPrime(i)){
+        if (i > result){
+            result = i;
         }
-        
     }
-
 }
+console.log('The largest prime factor of the number', num, 'is', result);
 
 function isPrime(x){
     for (let i = 2; i <= Math.sqrt(x); ++i){
-        if (x % i == 2){
+        if (x % i == 0){
             return false;
         }
     }
